2901b414433c686dd60dde3b4d0237cdb0d72321
armandouriser <42256605+armandouriser@users.noreply.github.com>
2019-02-20 11:15:33 -0600

    Add files via upload

---
+++ ejercicio 1/MyPlayground1.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+let numberOfTickets = 150
+let precio = 10
+let room = 1000
+let poster = 40
+let mult = numberOfTickets * precio
+let sum = room + poster
+let total = mult - sum
+print(total)
+
---
+++ ejercicio 1/ejercicio1.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var numberOfTickets = 150
+var precio = 10
+var room = 1000
+var poster = 40
+var mult = numberOfTickets * precio
+var sum = room + poster
+var total = mult - sum
+print(total)
