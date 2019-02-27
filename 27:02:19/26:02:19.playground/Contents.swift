bee2aabc6ff7f36b645dcedb1cfbfff44e9bf3d3
armandouriser <42256605+armandouriser@users.noreply.github.com>
2019-02-27 11:24:32 -0600

    Add files via upload

---
+++ 27:02:19/26:02:19.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+func rowTheBoat() {
+    
+    print("Row, row, row your boat")
+    print("Gently down the stream")
+}
+
+func merrilyDream() {
+    
+    print("Merrily, merrily, merrily, merrily")
+    print("Life is but a dream")
+}
+
+func verseOne() {
+
+    rowTheBoat()
+    merrilyDream()
+   
+}
+
+verseOne()
+
