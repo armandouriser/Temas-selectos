28e141c5540c69d1c5ff397a1e63378d0c851ff7
armandouriser <42256605+armandouriser@users.noreply.github.com>
2019-04-24 11:33:11 -0500

    Add files via upload

---
+++ EjercicioTipoExamen/EjercicioTipoExamen/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EjercicioTipoExamen
+//
+//  Created by Macbook on 4/24/19.
+//  Copyright © 2019 ClaseMPOO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Text: UITextField!
+    @IBAction func boton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+       
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func ChangeColor(_ sender: Any) {
+    }
+}
+
